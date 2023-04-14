462889ae9f2c2d41ba5d3bed180e4084ee83eac7
Martha <martha.maina@student.moringaschool.com>
2023-04-14 17:50:08 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const taskForm = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('#task-list');
 
-taskForm.addEventListener('submit', function(event) {
-
+function addTask(event) {
   event.preventDefault();
+  const task = taskInput.value.trim();
+  if (task === '') {
+    return;
+  }
+  const taskItem = document.createElement('li');
+  taskItem.textContent = task;
+  taskList.appendChild(taskItem);
+  taskInput.value = '';
+}
 
- 
-});
+taskForm.addEventListener('submit', addTask);
